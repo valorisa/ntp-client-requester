098e56f3d9f67ad535b582dbd8a914a93cd69137
valorisa <bbrodeau@gmail.com>
2025-02-22 04:17:00 +0100

    Ajout des fichiers de configuration, du client NTP et du Dockerfile

--- src/ntp-client.ts
+++ src/ntp-client.ts
@@ -1,26 +1,31 @@
 import { createSocket, Socket } from 'dgram';
 
-class NTPClient { private readonly ntpServer: string; private socket: Socket;
+class NTPClient {
+  private readonly ntpServer: string;
+  private socket: Socket;
 
-constructor(server: string = 'pool.ntp.org') { this.ntpServer = server; this.socket = createSocket('udp4'); }
+  constructor(server: string = 'pool.ntp.org') {
+    this.ntpServer = server;
+    this.socket = createSocket('udp4');
+  }
 
-async getNetworkTime(): Promise<Date> { return new Promise((resolve, reject) => { const ntpData = Buffer.alloc(48); ntpData[0] = 0x1B; // NTP version 4, mode client
+  async getNetworkTime(): Promise<Date> {
+    return new Promise((resolve, reject) => {
+      const ntpData = Buffer.alloc(48);
+      ntpData[0] = 0x23; // LI=0, Version=4, Mode=3 (Client)
 
-this.socket.send(ntpData, 123, this.ntpServer, (err) => {
-    if (err) reject(err);
-  });
+      this.socket.send(ntpData, 123, this.ntpServer, (err) => {
+        if (err) reject(err);
+      });
 
-  this.socket.once('message', (msg) => {
-    const offsetTransmitTime = 40;
-    const seconds = msg.readUInt32BE(offsetTransmitTime) - 2208988800; // Convertir en epoch Unix
-    const milliseconds = msg.readUInt32BE(offsetTransmitTime + 4) * 1000 / 2 ** 32;
-    resolve(new Date((seconds * 1000) + milliseconds));
-  });
-});
-
-} }
+      this.socket.once('message', (msg) => {
+        const offsetTransmitTime = 40;
+        const seconds = msg.readUInt32BE(offsetTransmitTime);
+        const ntpEpoch = Date.UTC(1900, 0, 1);
+        resolve(new Date(ntpEpoch + seconds * 1000));
+      });
+    });
+  }
+}
 
 export default NTPClient;
-
-// Exemple d'utilisation (async () => { const client = new NTPClient(); try { const networkTime = await client.getNetworkTime(); console.log('Heure réseau:', networkTime.toISOString()); } catch (error) { console.error('Erreur lors de la récupération de l'heure:', error); } })();
-
